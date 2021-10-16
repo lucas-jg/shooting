5cab30269e3f44f2eb1317df44a1454a8dc56b7a
Lucas <lucas_backend@kakao.com>
2021-10-16 16:36:46 +0900

    Add player collider2D and move functions.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,49 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
-    private void Update() {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+    private float collideHorizontal = 1;
+    private float collideVertical = 1;
+
+    private void Update()
+    {
+
+        float h = Input.GetAxisRaw("Horizontal") * collideHorizontal;
+        float v = Input.GetAxisRaw("Vertical") * collideVertical;
 
         Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
         transform.position += nextPos;
+
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Border")
+        {
+            if (other.gameObject.name == "Vertical")
+            {
+                collideVertical = 0;
+            }
+            else if (other.gameObject.name == "Horizontal")
+            {
+                collideHorizontal = 0;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Border")
+        {
+            if (other.gameObject.name == "Vertical")
+            {
+                collideVertical = 1;
+            }
+            else if (other.gameObject.name == "Horizontal")
+            {
+                collideHorizontal = 1;
+            }
+        }
+    }
+
+
 }
